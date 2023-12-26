5c7f3fb897a4f072559a73f69e853844081f3aa9
davidperkins <davidjperkins@outlook.com>
2023-12-26 23:55:13 +0000

    Add model dropdown continued

--- controllers/display.js
+++ controllers/display.js
@@ -4,16 +4,30 @@ exports.addModel = (req, res, next) => {
 
 	Promise.all([
 		Grls.getAttributeList('ASHP'),
-		Grls.getAttributeList('NATN'),
+		Grls.getAttributeList('ASIZ'),
+		Grls.getAttributeList('ATTR'),
+		Grls.getAttributeList('BILD'),
+		Grls.getAttributeList('BRDR'),
+		Grls.getAttributeList('BRSH'),
+		Grls.getAttributeList('BSIZ'),
+		Grls.getAttributeList('CMPX'),
 		Grls.getAttributeList('EYES'),
-		Grls.getAttributeList('HAIR')
+		Grls.getAttributeList('HAIR'),
+		Grls.getAttributeList('NATN')
 	])
 	.then((arr) => {
 		res.render('main-page/model-add', {
 			ashp_list: arr[0],
-			natn_list: arr[1],
-			eyes_list: arr[2],
-			hair_list: arr[3],
+			asiz_list: arr[1],
+			attr_list: arr[2],
+			bild_list: arr[3],
+			brdr_list: arr[4],
+			brsh_list: arr[5],
+			bsiz_list: arr[6],
+			cmpx_list: arr[7],
+			eyes_list: arr[8],
+			hair_list: arr[9],
+			natn_list: arr[10],
 			pageTitle: 'Add Model',
 			path: '/add-model'
 		});
